173b14cb011b8ec91b807fc24d19f9738d26efa0
valdirnerin <valdirnerin@hotmail.com>
2025-08-03 19:46:11 -0300

    chore: specify Mercado Pago production key

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-// Mercado Pago public key
+// Mercado Pago public key (production)
 window.MP_PUBLIC_KEY = 'APP_USR-c28b783a-54c0-4e39-80d3-f8c7dae2b645';
 window.API_BASE_URL = 'https://ecommerce-3-0.onrender.com';
